7c2ee66a453d97f392a0782b66cdb4ffd1391016
Saran Prasath <120230107+Saranprasathr@users.noreply.github.com>
2024-12-28 14:57:25 +0530

    Add files via upload

---
+++ academics.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Computer Science</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+		
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Cyber Security</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Information Technology</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Artificial Intelligence & Data Science</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+						
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
