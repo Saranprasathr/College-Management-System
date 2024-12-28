ef92f0edb29ccffc0f735a0013a96dd34d1ffdc0
Saran Prasath <120230107+Saranprasathr@users.noreply.github.com>
2024-12-28 15:06:00 +0530

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,33 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>PROGRAMS OFFERED</h2>
+            <ul>
+              <li><a href="">B.Sc. Artificial Intelligence and Machine Learning</a></li>
+              <li><a href="">B.Sc. Information Technology with Data Science</a></li>
+              <li><a href="">B.Sc. Computer Science with Cloud Technology and Information Security </a></li>
+              <li><a href="">B.Sc. Computer Technology with Blockchain</a></li>
+              <li><a href="">B.Sc. Computer Science with Data Science</a></li>
+              <li><a href="">B.Sc. Artificial Intelligence and Data Science</a></li>
+              <li><a href="">B.Sc. Computer Technology with Full Stack Developement</a></li>
+              <li><a href="">B.Sc. Artificial Intelligence with Cyber Security</a></li>
+              <li><a href="">B.Sc. Digital Cyber Forensic Science</a></li>
+              <li><a href="">B.Sc. Computer Science with Cyber Security</a></li>
+              <li><a href="">M.Sc. Data Science with Business Analytics</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Rathinam Techzone Campus,
+                Pollachi Main Road, Eachanari, Coimbatore-641021
+              </p>
+              <p>Telephone: 0422-4040906</p>
+              <p>E-mail: info@rathinam.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © Alright Reserve 2024</p>
+</footer>
